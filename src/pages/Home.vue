ba7eb3577fdca8cef68d8eb56674265ea7c55498
aum <sweetcandy.aum@gmail.com>
2021-06-05 18:57:42 +0700

    add 4 card

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,17 +1,68 @@
 <template>
   <q-layout view="hHh lpR fFf">
     <q-page-container>
-    <h5>Home Page</h5>
+      <h5>Home Page</h5>
     </q-page-container>
+    <div class="q-pa-md row items-start q-gutter-md">
+      <div class="row">
+        <div class="col">
+          <q-card bordered class="bg-grey-9 my-card">
+            <q-card-section>
+              <p>ใส่ภาพ</p>
+            </q-card-section>
+
+            <q-separator dark inset />
+            <q-card-actions>
+              <q-btn to="myfarm" flat>สวนของฉัน</q-btn>
+            </q-card-actions>
+          </q-card>
+        </div>
+        <div class="col">
+          <q-card bordered class="bg-grey-9 my-card">
+            <q-card-section>
+              <p>ใส่ภาพ</p>
+            </q-card-section>
+
+            <q-separator dark inset />
+            <q-card-actions>
+              <q-btn to="weather" flat>สภาพอากาศ</q-btn>
+            </q-card-actions>
+          </q-card>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <q-card bordered class="bg-grey-9 my-card">
+            <q-card-section>
+              <p>ใส่ภาพ</p>
+            </q-card-section>
+
+            <q-separator dark inset />
+            <q-card-actions>
+              <q-btn to="weather" flat>สรุปบัญชี</q-btn>
+            </q-card-actions>
+          </q-card>
+        </div>
+        <div class="col">
+          <q-card bordered class="bg-grey-9 my-card">
+            <q-card-section>
+              <p>ใส่ภาพ</p>
+            </q-card-section>
+
+            <q-separator dark inset />
+            <q-card-actions>
+              <q-btn to="weather" flat>รายได้รายจ่าย</q-btn>
+            </q-card-actions>
+          </q-card>
+        </div>
+      </div>
+    </div>
   </q-layout>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>

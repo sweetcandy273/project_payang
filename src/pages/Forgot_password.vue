9389aa0e9c2cb375ff9e16b47e2b8dcc75a74260
aum <sweetcandy.aum@gmail.com>
2021-06-05 18:10:26 +0700

    add forgot password page

---
+++ src/pages/Forgot_password.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-page-container>
+    <h5>Forgot_password Page</h5>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

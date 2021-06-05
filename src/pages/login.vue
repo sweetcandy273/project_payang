5bd717a5b4b66a7ac4c1afa8e5e6c64e83e9d284
aum <sweetcandy.aum@gmail.com>
2021-06-05 17:52:41 +0700

    adjust q-page tag

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,48 +1,46 @@
 <template>
-  <q-layout view="hHh lpR fFf">
-    <q-page-container>
-      <div class="row q-gutter-xs">
-        <img
-          alt="Payang logo"
-          src="~assets/payang_icon.png"
-          style="width: 200px; height: 200px"
-        />
-        <h5>Payang App</h5>
+  <q-page class="flex flex-center">
+    <div class="row q-gutter-xs">
+      <img
+        alt="Payang logo"
+        src="~assets/payang_icon.png"
+        style="width: 200px; height: 200px"
+      />
+      <h5>Payang App</h5>
 
-        <div class="row">
-          <q-input
-            color="teal"
-            filled
-            v-model="id"
-            label="เบอร์โทรศัพท์ / อีเมลล์"
-          >
-          </q-input>
-          <q-input color="teal" filled v-model="password" label="รหัสผ่าน">
-          </q-input>
-        </div>
-        <div class="row">
-            <router-link to="/register">สมัครสมาชิก</router-link>
-          <q-btn
-            to="/login"
-            unelevated
-            rounded
-            color="green-6"
-            label="เข้าสู่ระบบ"
-          />
-        </div>
+      <div class="row">
+        <q-input
+          color="teal"
+          filled
+          v-model="id"
+          label="เบอร์โทรศัพท์ / อีเมลล์"
+        >
+        </q-input>
+        <q-input color="teal" filled v-model="password" label="รหัสผ่าน">
+        </q-input>
       </div>
-    </q-page-container>
-  </q-layout>
+      <div class="row">
+        <router-link to="/register">สมัครสมาชิก</router-link>
+        <q-btn
+          to="/login"
+          unelevated
+          rounded
+          color="green-6"
+          label="เข้าสู่ระบบ"
+        />
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script>
 export default {
-    data(){
-       return {
+  data() {
+    return {
       id: "",
       password: "",
     };
-    }
+  },
 };
 </script>
 

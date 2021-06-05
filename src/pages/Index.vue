02c574acbe97b0e396ad979baaa01e53fc9ca88b
aum <sweetcandy.aum@gmail.com>
2021-06-05 17:53:50 +0700

    format doc

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,11 +7,9 @@
         style="width: 200px; height: 200px"
       />
       <h5>Payang App</h5>
-     <p>ใส่ข้อมูล</p>
-<q-btn to="/login" unelevated rounded color="green-6" label="เริ่มต้น" />
-
+      <p>ใส่ข้อมูล</p>
+      <q-btn to="/login" unelevated rounded color="green-6" label="เริ่มต้น" />
     </div>
-
 
     <!-- เพิ่มทุกอย่างหน้านี้ -->
   </q-page>

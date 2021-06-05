437e19188e9ffa11b9b473400ed887c8ac591d93
aum <sweetcandy.aum@gmail.com>
2021-06-05 17:26:13 +0700

    set base first app

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,16 +1,24 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Payang logo"
-      src="~assets/payang_icon.png"
-      style="width: 200px; height: 200px"
-    >
+    <div class="row">
+      <img
+        alt="Payang logo"
+        src="~assets/payang_icon.png"
+        style="width: 200px; height: 200px"
+      />
+      <h5>Payang App</h5>
+     <p>ใส่ข้อมูล</p>
+<q-btn to="/login" unelevated rounded color="green-6" label="เริ่มต้น" />
 
+    </div>
+
+
+    <!-- เพิ่มทุกอย่างหน้านี้ -->
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
-}
+  name: "PageIndex",
+};
 </script>

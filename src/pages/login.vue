c1e55a90afe1e200ddf2d9c4f79e665e588b5582
aum <sweetcandy.aum@gmail.com>
2021-06-05 17:47:15 +0700

    add  router-link to register

---
+++ src/pages/login.vue
@@ -0,0 +1,50 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-page-container>
+      <div class="row q-gutter-xs">
+        <img
+          alt="Payang logo"
+          src="~assets/payang_icon.png"
+          style="width: 200px; height: 200px"
+        />
+        <h5>Payang App</h5>
+
+        <div class="row">
+          <q-input
+            color="teal"
+            filled
+            v-model="id"
+            label="เบอร์โทรศัพท์ / อีเมลล์"
+          >
+          </q-input>
+          <q-input color="teal" filled v-model="password" label="รหัสผ่าน">
+          </q-input>
+        </div>
+        <div class="row">
+            <router-link to="/register">สมัครสมาชิก</router-link>
+          <q-btn
+            to="/login"
+            unelevated
+            rounded
+            color="green-6"
+            label="เข้าสู่ระบบ"
+          />
+        </div>
+      </div>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+    data(){
+       return {
+      id: "",
+      password: "",
+    };
+    }
+};
+</script>
+
+<style>
+</style>

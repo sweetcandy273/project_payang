7117009173b2c87bf2316f2aab761d17bc084e6c
aum <sweetcandy.aum@gmail.com>
2021-06-05 17:08:12 +0700

    adjust logo

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,11 @@
 <template>
   <q-page class="flex flex-center">
     <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
+      alt="Payang logo"
+      src="~assets/payang_icon.png"
       style="width: 200px; height: 200px"
     >
+
   </q-page>
 </template>
 

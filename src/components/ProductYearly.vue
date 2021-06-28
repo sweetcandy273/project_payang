0c7daa087fe7508466172fef14867bf0402ece61
aum <sweetcandy.aum@gmail.com>
2021-06-28 13:41:11 +0700

    test about  doughnut2d for chart in home

--- src/components/ProductYearly.vue
+++ src/components/ProductYearly.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <div class="font" style="font-size: 30px">2021</div>
     <div class="row font q-px-md seft-center text-center">
       <div class="col-2 seft-center">
@@ -35,7 +35,7 @@
         <div class="row">
           <div class="col-10">
             <div style="font-size: 25px">สรุปบัญชีรวมทั้งหมด</div>
-            <div>ประจำปี</div>
+            <div style="font-size: 20px">ประจำปี</div>
           </div>
           <div class="col">
             <div>
@@ -49,7 +49,23 @@
             <div>บัญชี</div>
           </div>
         </div>
-        <div>กราฟ</div>
+        <div>
+          <!-- <div style="font-size: 25px">2021</div> -->
+          <div id="app">
+            <div id="chart-container">
+              <fusioncharts
+             
+                :type="type"
+                :width="width"
+                :height="height"
+                :dataformat="dataFormat"
+                :dataSource="dataSource"
+              >
+              </fusioncharts>
+            </div>
+          </div>
+        </div>
+
         <div class="row items-end">
           <div class="col text-left" style="font-size: 20px; color: #06be3b">
             <div>รายรับ</div>
@@ -82,17 +98,66 @@
           <div class="col-1 text-right" style="font-size: 19px">
             <div>บ.</div>
           </div>
-        
         </div>
         <div>
-        <hr style="height:2px;border-width:0; color:#4E7971; background-color:#4E7971">
+          <hr
+            style="
+              height: 2px;
+              border-width: 0;
+              color: #4e7971;
+              background-color: #4e7971;
+            "
+          />
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
-export default {};
+import Vue from "vue";
+import VueFusionCharts from "vue-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Doughnut2d from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+Vue.use(VueFusionCharts, FusionCharts, Doughnut2d, FusionTheme);
+
+const chartData = [
+  {
+    label: "รายรับ",
+    value: "100",
+  },
+  {
+    label: "รายจ่าย",
+    value: "20",
+  },
+];
+const dataSource = {
+  chart: {
+    showpercentvalues: "1",
+    defaultcenterlabel: "2021",
+    aligncaptionwithcanvas: "0",
+    captionpadding: "0",
+    decimals: "1",
+  
+    theme: "fusion",
+  },
+  data: chartData,
+};
+
+export default {
+  name: "app",
+  data() {
+    return {
+      type: "doughnut2d",
+
+      width: "100%",
+      height: "300",
+      dataFormat: "json",
+      dataSource,
+    };
+  },
+};
 </script>
 
 <style scoped>
@@ -115,6 +180,5 @@ export default {};
   background-color: #c5dba8;
   border-radius: 20px;
 }
-
 
 </style>

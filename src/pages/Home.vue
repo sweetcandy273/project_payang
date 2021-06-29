f080ff9c87ab9b8b0422cb73e53b70c78bddf13c
Orrathai Oongsakun <hat.gung.2015@gmail.com>
2021-06-29 20:00:51 +0700

    testpage home from aum

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,55 +1,103 @@
 <template>
   <div>
-    <q-layout
-      view="lHh lpr lFf"
-      container
-      style="height: 1000px"
-      class="shadow-2 rounded-borders"
-    >
-      <q-header>
-        <q-toolbar>
-          <q-space></q-space>
-          <q-btn flat round dense icon="search" class="q-mr-xs" />
-          <q-btn flat round dense icon="group_add" />
-        </q-toolbar>
-        <q-toolbar class="text-center row">
-          <div class="col self-center">ตั้งค่า</div>
+    <q-header class="shadow-2">
+      <!-- <q-toolbar>
+        <q-space></q-space>
+        <q-btn flat round dense icon="search" class="q-mr-xs" />
+        <q-btn flat round dense icon="group_add" />
+      </q-toolbar> -->
+      <q-toolbar class="text-center row">
+        <div class="col self-center font">ตั้งค่า</div>
 
-          <div class="col-6 pya">หน้าหลัก</div>
-          <div class="col self-center"></div>
-        </q-toolbar>
-      </q-header>
+        <div class="col-6 font header-title">หน้าหลัก</div>
+        <div class="col self-center"></div>
+      </q-toolbar>
+    </q-header>
 
-      <div>
-        <div class="q-pa-md">
-          <div class="row text-center">
-            <div class="col">
-              <div class="pya">
-                Payang App
-                <q-icon name="help" />
-              </div>
-            </div>
-          </div>
-          <div class="row text-center">
-            <div class="col pya">คุณชนิกานต์</div>
+    <div class="q-px-md">
+      <div class="q-pa-md">
+        <div class="row text-center">
+          <div class="col font" style="font-size: 25px">
+            Payang App
+            <q-icon name="info" style="color: #5db075" />
           </div>
         </div>
+        <div class="row text-center">
+          <div class="col font" style="font-size: 24px">คุณชนิกานต์</div>
+        </div>
       </div>
-    </q-layout>
+
+      <div class="row myfarm font shadow-2">
+        <div class="col-6 q-pa-md self-center">
+          <div style="font-size: 20px">สวนของฉัน</div>
+        </div>
+        <div class="col q-px-md text-right self-center">
+          <img
+            class="logo"
+            alt="Payang logo"
+            src="../assets/forest.png"
+            style="width: 80px"
+          />
+        </div>
+      </div>
+      <div class="q-pt-md text-center font" style="font-size: 20px">ผลผลิต</div>
+      <div class="q-py-md">
+        <q-btn-toggle
+          class="shadow-2"
+          v-model="secondModel"
+          spread
+          @click="toggle(secondModel)"
+          no-caps
+          rounded
+          unelevated
+          toggle-color="grey-8"
+          color="grey-5"
+          text-color="white"
+          :options="[
+            { label: 'ปี', value: 'yearly' },
+            { label: 'เดือน', value: 'monthly' },
+          ]"
+        />
+      </div>
+    </div>
+
+    <div>
+      <div v-if="secondModel == 'yearly'" class="text-center" id="yearly">
+        <ProductYearly />
+      </div>
+      <div v-else class="text-center">
+        <ProductMonthly />
+      </div>
+    </div>
   </div>
 </template>
 
+
 <script>
+import ProductYearly from "../components/ProductYearly.vue";
+import ProductMonthly from "../components/ProductMonthly.vue";
+
+export default {
+  name: "yearly",
+  components: {
+    ProductYearly,
+    ProductMonthly,
+  },
+
+  data() {
+    return {
+      model: null,
+      secondModel: "yearly",
+    };
+  },
+  methods: {
+    toggle(data) {
+      // console.log(data);
+    },
+  },
+};
 </script>
 
 
-
-<style>
-/* *{
-  background: #dae5de;
-} */
-
-.pya {
-  font-size: 25px;
-}
+<style scoped src="../css/home.css">
 </style>

c5a67c952a3c8bda8c173e54fb106fda3c741891
Chanikan <chanikan152543@gmail.com>
2021-07-17 23:50:14 +0700

    Create Detail_income.vue

---
+++ src/pages/Detail_income.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <q-header>
+      <!-- <q-toolbar>
+        <q-space></q-space>
+      </q-toolbar>  -->
+      <q-toolbar class="text-center row">
+        <div class="col self-center">
+          <img class="close" src="../assets/back-left.png" style="width: 50px" />
+        </div>
+
+        <div class="col-6 font header-title">รายละเอียด</div>
+        <div class="col self-center"></div>
+      </q-toolbar>
+    </q-header>
+  
+        
+   
+ </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+     
+    };
+  },
+};
+</script>
+<style scoped src="../css/home.css">
+</style>

1a5df23e6262e7b8a6056743ad79c56d6a4d2526
aum <sweetcandy.aum@gmail.com>
2021-07-17 22:32:41 +0700

    add formmm

--- src/pages/Edit_userinformation.vue
+++ src/pages/Edit_userinformation.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <q-header class="shadow-2">
+    <q-header class="shadow-2">
       <!-- <q-toolbar>
         <q-space></q-space>
         <q-btn flat round dense icon="search" class="q-mr-xs" />
@@ -15,46 +15,107 @@
         </div> -->
         <div
           class="col self-center font"
-         @click="$router.push({ name: 'home' })"
+          @click="$router.push({ name: 'home' })"
         >
-         กลับ
+          กลับ
         </div>
 
         <div class="col-6 font header-title">ข้อมูลผู้ใช้</div>
         <div class="col self-center"></div>
       </q-toolbar>
     </q-header>
+    <div class="q-pa-md font">
+      <q-form @submit="onSubmit" class="q-gutter-md">
+        <q-input
+          v-model="fname"
+          label="ชื่อ"
+          :rules="[(val) => (val && val.length > 0) || 'กรอกไอสัส']"
+        />
+        <q-input
+          v-model="lname"
+          label="นามสกุล"
+          :rules="[(val) => (val && val.length > 0) || 'กรุณากรอกนามสกุล']"
+        />
+        <q-input
+          v-model="phone_number"
+          label="เบอร์โทรศัพท์"
+          :rules="[(val) => (val && val.length > 0) || 'กรุณากรอกเบอร์โทรศัพท์']"
+        />
+        <q-input v-model="email" label="อีเมล" 
+        :rules="[(val) => (val && val.length > 0) || 'กรุณากรอกอีเมล']"
+        />
+        <q-input
+          v-model="address"
+          label="ที่อยู่ (บ้านเลขที่ หมู่ที่ ตรอก/ซอย แขวง/ตำบล)"
+          :rules="[(val) => (val && val.length > 0) || 'กรุณากรอกที่อยู่']"
+        />
+        <q-input v-model="address_district" label="เขต/อำเภอ" 
+        :rules="[(val) => (val && val.length > 0) || 'กรุณากรอกเขต/อำเภอ']"
+        />
+        <q-input v-model="address_province" label="จังหวัด"
+        :rules="[(val) => (val && val.length > 0) || 'กรุณากรอกจังหวัด']"
+         />
+        <q-input v-model="zip_code" label="รหัสไปรษณีย์ " 
+        :rules="[(val) => (val && val.length > 0) || 'กรุณากรอกรหัสไปรษณีย์']"
+        />
 
-
-
-
+        <div>
+          <q-btn unelevated rounded color="primary" label="ยืนยัน" type="submit"/>
+        
+        </div>
+      </q-form>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import Vue from "vue";
+import VueCompositionAPI from "@vue/composition-api";
 
-}
+Vue.use(VueCompositionAPI);
+import { ref } from "@vue/composition-api";
+
+export default {
+  setup() {
+    return {
+      fname: ref("ชนิกานต์"),
+      lname: ref("ปิยะพงษ์"),
+      phone_number: ref("082-222-2222"),
+      email: ref("payang01@gmail.com"),
+      address: ref("112/1 ซอยหล่อโรง ถนนระนอง ตำบลตลาดเหนือ "),
+      address_district: ref("เมืองภูเก็ต"),
+      address_province: ref("ภูเก็ต"),
+      zip_code: ref("83000"),
+    };
+  },
+  methods: {
+    onSubmit() {
+      //บันทึกข้อมูลลง database ใช้ this.ตัวแปร น้าาา
+      console.log(this.fname);
+    },
+  },
+};
 </script>
 
 <style>
-
 .bg {
-    background: #dae5de;
+  background: #dae5de;
 }
-
-
 
 .q-toolbar {
-    background-color: #4e7971;
+  background-color: #4e7971;
 }
 
-.header-title{
-    font-size: 25px;
+.header-title {
+  font-size: 25px;
 }
 
-.font{
-    font-family: 'Kanit', sans-serif;
-    
+.font {
+  font-family: "Kanit", sans-serif;
+}
+
+.q-btn{
+size: 100%;
+background-color: #4e7971;
 }
 </style>

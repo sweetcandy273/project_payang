6e0bb0a040591db1481154c0cf38c23ce7d14eaa
Chanikan <chanikan152543@gmail.com>
2021-07-25 22:09:29 +0700

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -18,7 +18,8 @@ const routes = [
       { path: 'edit_expenditure', name: 'edit_expenditure', component: () => import('pages/Edit_expenditure.vue') },
       { path: 'detail_income', name: 'detail_income', component: () => import('pages/Detail_income.vue') },
       { path: 'edit_userinformation', name: 'edit_userinformation', component: () => import('pages/Edit_userinformation.vue') },
-     
+      { path: 'detail_income', name: 'detail_income', component: () => import('pages/Detail_income.vue') },
+      { path: 'detail_expenditure', name: 'detail_expenditure', component: () => import('pages/Detail_expenditure.vue') },
 
 
     ]

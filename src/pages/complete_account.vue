85b8689810a02f38e9b002a4ed6c4fdd76fb427a
Chanikan <chanikan152543@gmail.com>
2021-07-25 19:54:04 +0700

    Create complete_account.vue

---
+++ src/pages/complete_account.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <q-header>
+      <!-- <q-toolbar>
+        <q-space></q-space>
+      </q-toolbar>  -->
+      <q-toolbar class="text-center row">
+        <div class="col-6 font header-title">เพิ่มบัญชี</div>
+        <div class="col self-center"></div>
+      </q-toolbar>
+    </q-header>
+<div class="complete">
+    
+</div>
+   
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+     
+    };
+  },
+};
+</script>
+<style scoped src="../css/home.css">
+</style>

ce391c27d73e27a48a191a5d90301c17885c7cc1
aum <sweetcandy.aum@gmail.com>
2021-07-29 17:53:02 +0700

    add plugins Loading

--- quasar.conf.js
+++ quasar.conf.js
@@ -79,7 +79,9 @@ module.exports = function (/* ctx */) {
     framework: {
       iconSet: 'material-icons', // Quasar icon set
       lang: 'en-us', // Quasar language pack
-      config: {},
+      config: {
+        loading:{}
+      },
 
       // Possible values for "importStrategy":
       // * 'auto' - (DEFAULT) Auto-import needed Quasar components & directives
@@ -95,7 +97,8 @@ module.exports = function (/* ctx */) {
 
       // Quasar plugins
       plugins: [
-        'Notify'
+        'Notify',
+        'Loading'
       ]
     },
 

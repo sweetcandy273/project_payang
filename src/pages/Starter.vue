3f1f37fac69d79ddba6ea0f4ee23bb51add610d7
aum <sweetcandy.aum@gmail.com>
2021-07-31 22:20:44 +0700

    add src for css & test about spining

--- src/pages/Starter.vue
+++ src/pages/Starter.vue
@@ -1,38 +1,75 @@
 <template>
-  <div>
-    <div class="fixed-center text-center">
-      <div>
-        <img
-          class="logo"
-          alt="Payang logo"
-          src="../assets/payang_icon.png"
-          style="width: 200px; height: 200px"
-        />
+  <div @click="showLoading">
+    <!-- <q-img src="../assets/bg-starter.png" width="100%"> -->
+    <div style="background: none">
+      <div class="fixed-center text-center">
+        <div>
+          <img
+            class="logo-starter"
+            alt="Payang logo"
+            src="../assets/payang_icon.png"
+          />
+        </div>
+        <div class="q-pt-xl">
+          <!-- <q-spinner color="green" size="3em"></q-spinner> -->
+        </div>
       </div>
-      <div class="q-pt-xl">
-        <q-spinner color="green" size="3em"></q-spinner>
-      </div>
-    </div>
 
-    <div class="fixed-bottom text-center q-pb-md">
-      <div>PayangTeam</div>
-      <div>@PSU Phuket</div>
+      <div class="fixed-bottom text-center q-pb-md text-black font">
+        <div>PayangTeam</div>
+        <div>@PSU Phuket</div>
+      </div>
     </div>
+    <!-- </q-img> -->
   </div>
 </template>
 
 <script>
+// import { onBeforeUnmount } from "vue";
 export default {
-  name: "PageIndex",
+  //  this.$router.push({
+  //     path:"/sterter_intro1",
+  //   }),
+
+  // setup() {
+  //   let timer;
+  //   onBeforeUnmount(() => {
+  //     if (timer !== void 0) {
+  //       clearTimeout(timer);
+  //       $q.loading.hide();
+  //     }
+  //   });
+  //   return {
+  //     showLoading() {
+  //       $q.loading.show({});
+
+  //       // hiding in 2s
+  //       timer = setTimeout(() => {
+  //         $q.loading.hide();
+  //         timer = void 0;
+  //       }, 2000);
+
+  //       $router.push({
+  //         path: "/starter_intro1",
+  //       });
+  //     },
+  //   };
+  // },
+
+  methods: {
+    showLoading() {
+      // this.$q.loading.show({
+      //   delay: 400, // ms
+      // });
+
+      // this.$q.loading.hide();
+      this.$router.push({
+        path: "/starter_intro1",
+      });
+    },
+  },
 };
 </script>
 
-<style>
-* {
-  background: #dae5de;
-}
-
-.logo {
-  border-radius: 10%;
-}
+<style scoped src="../css/home.css">
 </style>

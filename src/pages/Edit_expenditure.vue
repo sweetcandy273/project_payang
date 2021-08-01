61b40e900fb1cd999912b2f67cb2959116ba7443
Chanikan <chanikan152543@gmail.com>
2021-08-01 21:04:04 +0700

    update

--- src/pages/Edit_expenditure.vue
+++ src/pages/Edit_expenditure.vue
@@ -6,7 +6,7 @@
       </q-toolbar>  -->
       <q-toolbar class="text-center row">
         <div class="col self-center">
-         <q-img
+          <q-img
             src="../assets/close.png"
             style="width: 22px; height: 22px"
             @click="$router.push({ name: 'account_calendar' })"
@@ -23,9 +23,9 @@
         class="col"
         unelevated
         rounded
-        style="background: #2d9cdb; color: white; opacity:0.5"
+        style="background: #2d9cdb; color: white; opacity: 0.5"
         label="รายรับ"
-        
+        @click="$router.push({ name: 'add_income' })"
       />
       <q-btn
         class="col"
@@ -33,7 +33,6 @@
         rounded
         style="background: #f2994a; color: white"
         label="รายจ่าย"
-
       />
     </div>
     <div class="q-px-md font">
@@ -55,85 +54,77 @@
         </template>
       </q-input>
     </div>
-    <div class="row q-pa-md q-gutter-md text-center">
-      <div class="col">
-        <q-img src="../assets/maintenance_sapling.png" style="" width="100px">
-          <h4 class="font text-center" style="font-size: 20px;">บำรุงรักษา</h4>
-        </q-img>
-      </div>
-      <div class="col">
-        <q-img src="../assets/equipment_rubber-cup.png" style="" width="100px">
-           <h4 class="font text-center" style="font-size: 20px;">วัสดุ-อุปกรณ์</h4>
-        </q-img>
-      </div>
+
+    <div class="q-py-md text-center">
+      <q-btn-toggle
+        v-model="type_expenditure"
+        toggle-color="green"
+        toggle-text-color="black"
+        rounded
+        unelevated
+        :options="[
+          { value: 'maintenance', slot: 'maintenance' },
+          { value: 'equipment', slot: 'equipment' },
+        ]"
+      >
+        <template v-slot:maintenance>
+          <div class="row items-center no-wrap">
+            <q-img src="../assets/maintenance_sapling.png" width="100px">
+              <h4 class="font text-center" style="font-size: 20px">
+                บำรุงรักษา
+              </h4>
+            </q-img>
+          </div>
+        </template>
+
+        <template v-slot:equipment>
+          <div class="row items-center no-wrap">
+            <q-img
+              src="../assets/equipment_rubber-cup.png"
+              style=""
+              width="100px"
+            >
+              <h4 class="font text-center" style="font-size: 20px">
+                วัสดุอุปกรณ์
+              </h4>
+            </q-img>
+          </div>
+        </template>
+      </q-btn-toggle>
     </div>
-    <div class="q-gutter-y-md q-px-md font" style="max-width: 100%">
-      <div class="row q-pt-md">
-        <div class="col">
-          <q-select
-            filled
-            v-model="type"
-            :options="optionstype"
-            label="ตัวเลือก"
-          />
-        </div>
-      </div>
-      <div class="row">
-        <div class="col">
-          <q-input filled v-model="totalprice" label="รวมจำนวนเงิน" />
-        </div>
-      </div>
-      <div class="stores">
-        <q-select filled v-model="store" :options="options" label="ร้านค้า" />
-      </div>
-      <div class="share">
-        <q-checkbox 
-          v-model="selectshare"
-          val="teal"
-          style="font-size: 16px"
-          label="กนกวรรณ ผู้รับผิดชอบ"
-        />
-      </div>
 
-      <div class="col">
-        <q-input filled v-model="note" label="บันทึก" />
+    <div>
+      <div
+        v-if="type_expenditure == 'maintenance'"
+        class="text-center"
+        id="maintenance"
+      >
+        <Maintenance />
       </div>
-
-      <div class="submit row q-gutter-sm flex-center font">
-       <q-btn
-          unelevated
-          rounded
-          label="บันทึก"
-          type="submit"
-          class="shadow-2 text-white"
-          style="width: 100%; background-color: #4e7971"
-          @click="$router.push({ name: 'check_expenditure' })"
-        />
+      <div v-else class="text-center">
+        <Equipment />
       </div>
     </div>
   </div>
 </template>
 <script>
+import Maintenance from "../components/Maintenance.vue";
+import Equipment from "../components/Equipment.vue";
 export default {
+  components: {
+    Maintenance,
+    Equipment,
+  },
+
   data() {
     return {
-
-      date:"2021/07/18",
-      weight_rubber: "",
-      percent: "",
-      text: "",
-      price_rubber: "",
+      type_expenditure: "maintenance",
+      date: "2021/07/18",
       totalprice: "",
-      store: null,
-      options: ["ดาวน้ำยางสด", "ไก่น้ำยางสด"],
-      note: "",
-      type: null,
-      optionstype: ["ปุ๋ย", "ตัดหญ้า", "ตัดกาฝาก"],
-      selectshare: false,
-      optionspercent: ["60:40", "55:45", "50:50"],
     };
   },
 };
 </script>
+
 <style scoped src="../css/home.css">
 </style>

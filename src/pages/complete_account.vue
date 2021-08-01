3022a20d7ce586622e8d1687112b03562ef5ab72
Chanikan <chanikan152543@gmail.com>
2021-08-01 17:57:39 +0700

    Update complete_account.vue

--- src/pages/complete_account.vue
+++ src/pages/complete_account.vue
@@ -4,27 +4,52 @@
       <!-- <q-toolbar>
         <q-space></q-space>
       </q-toolbar>  -->
-      <q-toolbar class="text-center row">
-        <div class="col-6 font header-title">เพิ่มบัญชี</div>
-        <div class="col self-center"></div>
+      <q-toolbar class="row">
+        <!-- <div class="col flex">
+          <img
+            src="../assets/close.png"
+            style="width: 22px; height: 22px"
+            @click="$router.push({ name: 'account_calendar' })"
+          />
+        </div> -->
       </q-toolbar>
     </q-header>
-
-    
-      
-        <div class="complete"></div>
-      
-   
-
-   
+    <div class="font q-ma-md">
+      <div class="row">
+        <div class="col q-pa-xl">
+          <p class="font text-center" style="font-size: 40px;">
+                บันทึกเรียบร้อย
+              </p>
+          <q-img
+              src="../assets/complete.png"
+              style=""
+              width="100%"
+              height= "100p%"
+            >
+              
+            </q-img>
+        </div>
+      </div>
+      <div class="row q-gutter-sm flex-center">
+        
+          <q-btn
+            unelevated
+            rounded
+            label="ย้อนกลับ"
+            type="back"
+            class="shadow-2 text-white"
+            style="width: 100%; background-color: #4e7971"
+            @click="$router.push({ name: 'account_calendar' })"
+          />
+        
+      </div>
+    </div>
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {
-     
-    };
+    return {};
   },
 };
 </script>
@@ -32,14 +57,13 @@ export default {
 </style>
 <style scoped>
 .complete {
-  
-  background: #EED491;
+  background: #eed491;
   border-radius: 100%;
-   position: absolute;
-    top: 100%;
-    left: 100%;
-    margin: -50% 50% 50% -50%;
-    width: 100%;
-    height: 100%;
+  position: absolute;
+  top: 100%;
+  left: 100%;
+  margin: -50% 50% 50% -50%;
+  width: 100%;
+  height: 100%;
 }
 </style>

dd25cd2a9360a9f5952db75dbfebf70f3396914b
Orrathai Oongsakun <hat.gung.2015@gmail.com>
2021-08-09 03:44:33 +0700

    สร้าง template sohk detail_farm

---
+++ src/pages/detail_farm.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>
+      <div>เจ้าของสวน : กัลย์สุตา ปิยะพงศ์</div>
+      <div>สวน : ภูเก็ต</div>
+      <div>ที่อยู่ : 112/1</div>
+      <div>เนื้อที่ปลูก : 1.1 ไร่</div>
+      <div>ผู้ดูแล : กนกวรรณ น้ำสงวน</div>
+    </div>
+  </div>
+</template>
+
+<style scoped src="../css/home.css">
+</style>

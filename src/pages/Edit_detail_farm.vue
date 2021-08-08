6d15d97afc34b68ea016a7e238cee36cf1535f4b
Orrathai Oongsakun <hat.gung.2015@gmail.com>
2021-08-09 04:59:30 +0700

    สร้างหัวเรื่องของหน้า  edite_detail_farm

---
+++ src/pages/Edit_detail_farm.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="font">
+    <q-header class="shadow-2">
+      <q-toolbar class="text-center row">
+        <div
+          class="col self-center font"
+          @click="$router.push({ name: 'myfarm' })"
+        >
+          <q-icon name="close" />
+        </div>
+
+        <div class="col-6 font header-title">ข้อมูลสวน</div>
+        <div class="col self-center"></div>
+      </q-toolbar>
+    </q-header>
+  </div>
+</template>

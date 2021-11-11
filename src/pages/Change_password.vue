ec6a782ab68ad6db0c08e91c0ce8384a22a9863e
sweetcandy273 <sweetcandy.aum@gmail.com>
2021-11-11 22:38:55 +0700

    new change password add function onsubmit for sendpasswordresetemail

---
+++ src/pages/Change_password.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <q-header class="shadow-2">
+      <q-toolbar class="row">
+        <div class="col flex">
+          <img
+            src="../assets/close.png"
+            style="width: 22px; height: 22px"
+            @click="$router.push({ name: 'home' })"
+          />
+        </div>
+
+        <div class="col-6 font header-title text-center">ตั้งรหัสผ่านใหม่</div>
+        <div class="col self-center"></div>
+      </q-toolbar>
+    </q-header>
+    <div class="font q-pa-md margin-box">
+      <q-form @submit="onSubmit" class="q-gutter-md">
+        <div>
+          <q-input
+            color="teal"
+            filled
+            v-model="email"
+            label="อีเมล"
+            :rules="[val => (val && val.length > 0) || 'กรุณากรอกหรืออีเมล']"
+          >
+          </q-input>
+        </div>
+
+        <div>
+          <q-btn
+            unelevated
+            rounded
+            label="ยืนยัน"
+            type="submit"
+            class="shadow-2 text-white"
+          />
+        </div>
+      </q-form>
+    </div>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+
+export default {
+  data() {
+    return {
+      email: ""
+    };
+  },
+  methods: {
+    onSubmit() {
+      firebase
+        .auth()
+        .sendPasswordResetEmail(this.email)
+        .then(() => {
+          alert("Email สำหรับการเปลี่ยนรหัสผ่านได้ถูกส่งไปแล้ว");
+          this.$router.push({
+            name: "login"
+          });
+        })
+        .catch(error => {
+          const errorCode = error.code;
+          if (errorCode == "auth/invalid-email") {
+            alert("Email ไม่ถูกต้องตามหลัก");
+          } else if (errorCode == "auth/user-not-found") {
+            alert("ไม่มีข้อมูล Email ในระบบ");
+          }
+        });
+    }
+  }
+};
+</script>
+
+<style scoped src="../css/home.css"></style>

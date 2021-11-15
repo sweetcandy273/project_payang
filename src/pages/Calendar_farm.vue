75d86bdc467d49368df65bec92cd92313f460462
Orrathai Oongsakun <hat.gung.2015@gmail.com>
2021-11-15 21:56:30 +0700

    get employee

--- src/pages/Calendar_farm.vue
+++ src/pages/Calendar_farm.vue
@@ -1,8 +1,7 @@
-
 <template>
   <div>
     <q-header>
-      <q-toolbar class="text-center row">
+      <q-toolbar class="text-center row font">
         <div class="col flexed text-left">
           <q-icon
             @click="
@@ -37,34 +36,51 @@
         </template>
 
         <template v-slot:after>
-          <div class="box_type">
-            <q-tab-panels
-              v-model="date"
-              animated
-              transition-prev="jump-up"
-              transition-next="jump-up"
-            >
-              <q-tab-panel name="2021/07/10">
-                <div class="row font">
-                  <div class="col" style="font-size: 22px">2021/07/10</div>
-                  <div>
-                    <q-icon
-                      name="create"
-                      class="text-teal"
-                      style="font-size: 2rem"
-                    />
-                  </div>
-                </div>
-
-                <div class="row font q-pt-md">
-                  <div class="col text-right" style="font-size: 18px">
-                    ตัดหญ้า
-                  </div>
-                </div>
-              </q-tab-panel>
-            </q-tab-panels>
+          <div class="col font q-mx-md q-mt-md" style="font-size: 22px">
+            {{ date }}
           </div>
-
+    <div :key="index" v-for="(data, index) in activity_in_farm">
+      <div class="row font q-pt-md">
+        <div class="greencircle"></div>
+        <div class="col q-ml-xs" style="font-size: 18px">รายรับ</div>
+        <div class="col text-right" style="font-size: 18px">
+          {{ data.amount }}
+        </div>
+      </div>
+      <div
+        class="calendar-income q-pa-md"
+        @click="
+          $router.push({
+            path: 'detail_income',
+            query: {
+              id: data.in_id,
+            },
+          })
+        "
+      >
+        <div class="row font">
+          <div class="col" style="font-size: 16px">ร้านดาว น้ำยางสด, แผ่น</div>
+          <div class="col-3 text-right" style="font-size: 16px">
+            {{ data.amount }}
+          </div>
+        </div>
+        <div class="row font" style="font-size: 16px">
+          น้ำยางสด {{ data.weight }} กก. {{ data.percen_rubber }} % แห้ง
+          {{ data.dry_rubber }}
+        </div>
+        <div class="row font" style="font-size: 16px">
+          ราคายาง {{ data.rubber_price }} บ./กก.
+        </div>
+        <div class="row font">
+          <div class="col" style="font-size: 16px">
+            {{ data.employee }} ส่วนแบ่ง {{ data.percen_split }}
+          </div>
+          <div class="col-3 text-right" style="font-size: 16px">
+            {{ data.amount_net }}
+          </div>
+        </div>
+      </div>
+    </div>
           <div class="add-account text-center fixed-bottom q-pa-xl">
             <q-btn
               unelevated
@@ -76,7 +92,14 @@
                 height: 50px;
               "
               icon="add"
-              @click="$router.push({ name: 'add_calender_farm' })"
+              @click="
+                $router.push({
+                  name: 'Add_calender_farm',
+                  query: {
+                    id: data.farm_id,
+                  },
+                })
+              "
             />
           </div>
         </template>
@@ -94,13 +117,49 @@ export default {
     return {
       splitterModel: 50,
       date: "",
-      events:[],
+      events: [],
     };
   },
-  mounted() {},
+  mounted() {
+    // this.getactivity();
+  },
   methods: {
     formatDate(dateString) {
       return date.formatDate(dateString, "YYYY/MM/DD");
+    },
+  },
+
+  // async getactivity() {
+  //   const { data } = await axios.get(
+  //     `http://localhost:3000/activity_in_farm/list/` + this.$route.query.id
+  //   );
+  //   this.listAllactivity = data.data;
+  //   this.date = this.formatDate(new Date());
+  //   const listEvent = data.data.map((data) => {
+  //     return this.formatDate(data.date);
+  //   });
+  //   this.events.push(...listEvent);
+  // },
+  async getEmployee() {
+    const { data } = await axios.get(
+      "http://localhost:3000/farm_has_employee/list/" + this.$route.query.id
+    );
+    this.listAllemployee = data.data;
+    this.date = this.formatDate(new Date());
+    const listEvent = data.data.map((data) => {
+      return this.formatDate(data.date_expenditure);
+    });
+    this.events.push(...listEvent);
+  },
+    watch: {
+    date(value) {
+      this.incomes = this.listAllincome.filter((data) => {
+        // console.log(data.date_income,"==",date.formatDate(value,"YYYY/MM/DD"));
+        return date.formatDate(value, "YYYY-MM-DD") == data.date;
+      });
+      this.expenditures = this.listAllexpenditure.filter((data) => {
+        return date.formatDate(value, "YYYY-MM-DD") == data.date_expenditure;
+      });
     },
   },
 };

8f7938d9532a5cab99a1fedd98fc6957c0a19ffc
Orrathai Oongsakun <hat.gung.2015@gmail.com>
2021-11-25 15:27:12 +0700

    create template

---
+++ src/pages/Edit_activity.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="font">
+    <q-form @submit="onSubmit">
+      <q-header>
+        <q-toolbar class="text-center row">
+          <div class="col flexed text-left">
+            <q-icon
+              @click="
+                $router.push({
+                  name: 'calender_farm',
+                  query: { id: $route.query.idf },
+                })
+              "
+              name="arrow_back_ios"
+              size="30px"
+            ></q-icon>
+          </div>
+
+          <div class="col-6 font header-title">แก้ไข</div>
+          <div class="col self-center"></div>
+        </q-toolbar>
+      </q-header>
+
+      <div class="box-dropActivitytype q-pa-md">
+        <q-input
+          filled
+          v-model="activity_in_farms.activity"
+          color="teal"
+          label="ประเภทกิจกรรม"
+        >
+        </q-input>
+      </div>
+
+      <div class="q-px-md font">
+        <q-input
+          color="teal"
+          filled
+          v-model="activity_in_farms.date"
+          mask="date"
+        >
+          <template v-slot:append>
+            <q-icon name="event" class="cursor-pointer" color="teal">
+              <q-popup-proxy
+                ref="qDateProxy"
+                transition-show="scale"
+                transition-hide="scale"
+              >
+                <q-date v-model="activity_in_farms.date" color="green">
+                  <div class="row items-center justify-end">
+                    <q-btn v-close-popup label="Close" color="white" flat />
+                  </div>
+                </q-date>
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
+      </div>
+            <div class="q-mt-lg text-center " >
+              <q-btn
+              round
+              style="background: #4e7971; color: white; width: 50px; height: 50px"
+              color="deep-orange-13"
+              icon="delete"
+              @click="
+                DeleteEvent();
+                $router.push({
+                  name: 'myfarm',
+                  query: { id: farm.user_id },
+                });
+              "
+            />
+         </div>
+
+      <div class="add-account self-center fixed-bottom q-pa-md">
+        <q-btn
+          unelevated
+          rounded
+          label="บันทึก"
+          class="shadow-2 text-white"
+          type="submit"
+          value="onSubmit"
+        />
+      </div>
+    </q-form>
+  </div>
+</template>
+
+
+
+<script>
+import axios from "axios";
+import { date } from "quasar";
+
+export default {
+  data() {
+    return {
+      activity_in_farms: {},
+    };
+  },
+  mounted() {
+    this.getactivity();
+  },
+  methods: {
+    formatDate(dateString) {
+      return date.formatDate(dateString, "YYYY/MM/DD");
+    },
+
+    async getactivity() {
+      const { data } = await axios.get(
+        "http://localhost:3000/activity_in_farm/findactivity/" +
+          this.$route.query.ida
+      );
+
+      this.activity_in_farms = data.data;
+      this.activity_in_farms.date = this.formatDate(this.activity_in_farms.date);
+    },
+
+    async onSubmit() {
+      console.log(this.activity_in_farms);
+      const { data } = await axios.put(
+        "http://localhost:3000/activity_in_farm/update/" + this.$route.query.id,
+        {
+          date: this.activity_in_farms.date,
+          activity: this.activity_in_farms.activity,
+        }
+      );
+      this.activity_in_farm = data.data.date;
+      this.$router.push({
+        path: "/calender_farm",
+        query: {
+          id: this.activity_in_farm.act_farm_id,
+        },
+      });
+    },
+  },
+};
+</script>
+<style scoped src="../css/home.css">
+</style>
+<style scoped>
